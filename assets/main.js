8bf0ab9aeb32cf05a157a91a0875a2a9cce208fa
Dinara203 <dinara.sir2003@gmail.com>
2023-09-07 13:07:29 +0300

    klslfd

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+// alert('Привет');
+let city = prompt ('Введите название города')
+let year = prompt ('Введите год образования города')
+let people = prompt ('Введите население')
+let years = 2023-year
+alert ('Городу '+ city+' исполнилось '+ Number(years)+' лет с момента его образования. Население: '+ Number(people)+'')
